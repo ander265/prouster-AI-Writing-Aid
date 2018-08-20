221245b7585f688ed76db318e7a3ec02c9393a82
Jared Anderson <jared.bonjean.anderson@gmail.com>
2018-08-20 16:53:26 -0700

    reset data index

--- clean_reddit_data.py
+++ clean_reddit_data.py
@@ -1,8 +1,11 @@
 import pandas as pd 
 
 
-def clean_reddit_data(directory):
+def clean_source_data(directory):
     data = pd.read_table(directory, header=None)
-    data['prefix'] = data[0].apply(lambda x: str(x).split(']')[0][1:])
-    data['prompt'] = data[0].apply(lambda x: str(x).split(']')[-1])
-    return data.drop(columns=0, inplace=True)
+    data['prefix'] = data[0].apply(lambda x: str(x).split(']')[0][1:].strip())
+    data['prompt'] = data[0].apply(lambda x: str(x).split(']')[-1].strip())
+    data.drop(columns=0, inplace=True)
+    data = data[-data['prefix'].isin(['IP','OT','PM','MP','PI','CC'])]
+    data.reset_index(inplace=True, drop=True)
+    return data
