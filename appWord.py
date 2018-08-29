4dfc9609b1b7db631105566229e8592239babec8
Jared Anderson <jared.bonjean.anderson@gmail.com>
2018-08-29 11:35:27 -0700

    switch debug mode

--- appWord.py
+++ appWord.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from pickle import load
 from keras.models import load_model
+import keras.backend
+import tensorflow as tf
 from download_model import download_model
 from get_pickle import get_pickle
 import textgeneratorN
@@ -13,13 +15,25 @@ tg = textgeneratorN.WordGenerator(
     documents_file='results/sequences.txt',
     token_file='results/tokenizer.pkl',
     key='docs')
+
+#text = tg.generate_seq()
 #print(tg)
 app = Flask(__name__)
+def init():
+    global model,graph
+    # load the pre-trained Keras model
+    model = load_model('results/model.h5')
+    graph = tf.get_default_graph()
 
 @app.route('/')
 def index():
     text = tg.generate_seq()
-    return render_template('website.html', neural_network_output=text)
+    return render_template('website.html', 
+                neural_network_output=text)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
+    print(("* Loading Keras model and Flask starting server...",
+    "please wait until server has fully started"))
+#    init()
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=False)
