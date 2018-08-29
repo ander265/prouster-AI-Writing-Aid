7d8bb48058ba17453702f55cfca83257ce3a6703
Jared Anderson <jared.bonjean.anderson@gmail.com>
2018-08-29 11:32:22 -0700

    revise for word-level rNN

--- download_model.py
+++ download_model.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 def download_model():
-    if not os.path.isfile('tg.h5'):
-        response = requests.get('https://s3-us-west-2.amazonaws.com/jared.b.anderson/tg.h5')
-        with open('tg.h5','wb') as f:
+    if not os.path.isfile('model.h5'):
+        response = requests.get('https://s3-us-west-2.amazonaws.com/jared.b.anderson/model.h5')
+        with open('model.h5','wb') as f:
             f.write(response.content)
